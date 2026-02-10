30ef8942b247e7ab72dea3565f8a7d1fb948fe0f
Egor Malyshev <ya.krumm@ya.ru>
2026-02-10 13:26:53 +0300

    #24 В подмодуль common добавлен EventPublicFeignClient

---
+++ main-service/common/src/main/java/malyshev/egor/feign/event/EventPublicFeignClient.java
@@ -0,0 +1,36 @@
+package malyshev.egor.feign.event;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import malyshev.egor.dto.event.EventFullDto;
+import malyshev.egor.dto.event.EventShortDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Validated
+@FeignClient(name = "event-service",
+        contextId = "eventPublicApiClient",
+        path = "/events")
+public interface EventPublicFeignClient {
+
+    @GetMapping
+    public List<EventShortDto> get(@RequestParam(value = "text", required = false) String text,
+                                   @RequestParam(value = "categories", required = false) List<Long> categories,
+                                   @RequestParam(value = "paid", required = false) Boolean paid,
+                                   @RequestParam(value = "rangeStart", required = false) String rangeStart,
+                                   @RequestParam(value = "rangeEnd", required = false) String rangeEnd,
+                                   @RequestParam(value = "onlyAvailable", defaultValue = "false") Boolean onlyAvailable,
+                                   @RequestParam(value = "sort", required = false) String sort,
+                                   @RequestParam(value = "from", defaultValue = "0") @PositiveOrZero int from,
+                                   @RequestParam(value = "size", defaultValue = "10") @Positive int size,
+                                   HttpServletRequest request);
+
+
+    @GetMapping("/{id}")
+    public EventFullDto getById(@PathVariable("id") Long id,
+                                HttpServletRequest request);
+}
