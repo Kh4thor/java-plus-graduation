2dd73c0388ddb0dd35287d714c53fceba4195707
Egor Malyshev <ya.krumm@ya.ru>
2026-02-26 14:51:54 +0300

    #1 Рефактоинг модуля stats. Добавлены подмодули aggregator, collector, analyzer, serialization, stats-client

--- stats/stats-client/src/main/java/malyshev/egor/ewm/stats/client/exceptions/StatsClientException.java
+++
@@ -1,7 +0,0 @@
-package malyshev.egor.ewm.stats.client.exceptions;
-
-public class StatsClientException extends RuntimeException {
-    public StatsClientException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
